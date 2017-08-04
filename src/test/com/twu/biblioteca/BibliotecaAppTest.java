19af3142f0f017eb4d24df7906c681b81fd99ff2
bywang <wangbaiyuan@outlook.com>
2017-08-04 20:21:48 +0800

    add test of check option is? valid

--- src/test/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,54 +1,57 @@
 package com.twu.biblioteca;
 
-import org.junit.Test; 
-import org.junit.Before; 
+import org.junit.Test;
+import org.junit.Before;
 import org.junit.After;
 
 import static org.junit.Assert.assertEquals;
 
-/** 
-* BibliotecaApp Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>Aug 3, 2017</pre> 
-* @version 1.0 
-*/ 
+/**
+ * BibliotecaApp Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>Aug 3, 2017</pre>
+ */
 public class BibliotecaAppTest {
 
     private BibliotecaApp app;
 
     @Before
 public void before() throws Exception {
-     app = new BibliotecaApp();
-} 
+        app = new BibliotecaApp();
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-/** 
-* 
-* Method: main(String[] args) 
-* 
-*/ 
+    /**
+     * Method: main(String[] args)
+     */
 @Test
 public void shouldListOptionCorrectly() throws Exception {
     assertEquals(
             "Please select an option :\n" +
-            "\t[1]: List books\n" +
-            "\t[2]: Check out Book\n" +
-            "\t[3]: Return book\n" +
-            "\t[4]: Quit\n",app.getOptionList());
-//TODO: Test goes here... 
+                    "\t[1]: List books\n" +
+                    "\t[2]: Check out Book\n" +
+                    "\t[3]: Return book\n" +
+                    "\t[4]: Quit\n",app.getOptionList());
+//TODO: Test goes here...
 }
 
 
-@Test
-    public void shouldShowCorrectWelcomeMSg() throws  Exception{
+    @Test
+public void shouldShowCorrectWelcomeMSg() throws Exception {
 
-    assertEquals("Welcome to Biblioteca!",app.getWelcomeMsg());
+        assertEquals("Welcome to Biblioteca!",app.getWelcomeMsg());
 
-}
+    }
 
+    @Test
+    public void shouldReturnValidIfSelectValidOption() {
+        assertEquals(true, app.isValidOption(1));
+        assertEquals(false, app.isValidOption(9));
+    }
 
 } 
