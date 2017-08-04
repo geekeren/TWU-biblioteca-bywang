6e4dff67818f8b84aa271f1e5142ed3faa7fe568
bywang <wangbaiyuan@outlook.com>
2017-08-04 17:00:19 +0800

    add test of book

---
+++ src/test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,37 @@
+package com.twu.biblioteca;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+
+
+import static org.junit.Assert.assertEquals;
+
+/** 
+* Book Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Aug 4, 2017</pre> 
+* @version 1.0 
+*/ 
+public class BookTest {
+
+    private Book book;
+
+    @Before
+public void before() throws Exception {
+    book = new Book("JAVA","Wangbaiyuan",2017);
+
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+@Test
+public void shouldGetBookDetailsMsg() throws Exception{
+
+    assertEquals("<<JAVA>> Wangbaiyuan @2017 ",book.getDetails());
+    }
+
+} 
